3c794c5e356e4a72b36c9281bdb5989f6cba9efb
Griffin Solot-Kehl <griffin@transposit.com>
2019-07-03 19:49:35 +0000

    Commit from Transposit IDE

--- src/webhook.js
+++ src/webhook.js
@@ -9,7 +9,7 @@
       };
 	}
 	else {
-      return api.run('this.helpdesk', {ticket: api.run('this.ticket', {text: body.text}), trigger_id: body.trigger_id});
+      return api.run('this.helpdesk', {trigger_id: body.trigger_id});
 	}
   return { status_code: 200 };
 }
