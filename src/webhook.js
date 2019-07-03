2ec39d19e9adb54ae0ae7b4f922a3ee724bcc994
Griffin Solot-Kehl <griffin@transposit.com>
2019-07-03 19:39:51 +0000

    Commit from Transposit IDE

--- src/webhook.js
+++ src/webhook.js
@@ -1,4 +1,4 @@
-({ http_event }) => { }
+({ http_event }) => {
 	// let body = JSON.parse(http_event.body);
 	// if (body.challenge) {
 	// // https://api.slack.com/events/url_verification
@@ -11,5 +11,5 @@
 	// else {
 	// //return api.run('this.helpdesk', {ticket: api.run('this.ticket', {text: body.message.text}), trigger_id: body.token});
 	// }
-  return ; { status_code: 200 };
+  return { status_code: 200 };
 }
