baa293ed03f9789ad36c617510db909f85d9975b
Griffin Solot-Kehl <griffin@transposit.com>
2019-07-08 23:51:52 +0000

    Commit from Transposit IDE

--- src/webhook.js
+++ src/webhook.js
@@ -1,6 +1,5 @@
 ({ http_event }) => {
   let body = http_event.parsed_body;
-  let email = 'test@email.com';
   api.run('this.helpdesk', {trigger_id: body.trigger_id, text: 'test'});
   return { status_code: 200 };
 }
