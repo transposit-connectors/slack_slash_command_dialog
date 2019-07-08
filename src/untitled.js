28f5dab9deaee5560de94379f510934d7609b02a
Griffin Solot-Kehl <griffin@transposit.com>
2019-07-08 18:18:37 +0000

    Commit from Transposit IDE

---
+++ src/untitled.js
@@ -0,0 +1,33 @@
+({slackBody}) => {
+  
+  const elements = [{
+    type: "textarea",
+    name: "input",
+    label: "Your input",
+    hint: "Write the text from below into this text box"
+    //min_length: text.length,
+  }, {
+    type: "textarea",
+    name: "original",
+    label: "Copy this",
+    value: text,
+    min_length: text.length
+  }];
+  
+  // const ts = Date.now();
+  const state = JSON.stringify({
+    ts: Date.now(),
+    recordId: rec.id
+  })
+  
+  const dialogObj = {
+    callback_id: "type_race",
+    notify_on_cancel: false,
+    title: "SlackRacer Speed Test",
+    elements,
+    state
+  };
+
+  const trigger_id = slackBody.trigger_id;
+  return api.run("slack_bot.open_dialog", { $body: { trigger_id, dialog: JSON.stringify(dialogObj) }});
+}
