5eb022adb3548e91423b2a018d5bf8cadce5ae23
Griffin Solot-Kehl <griffin@transposit.com>
2019-07-08 18:10:29 +0000

    Commit from Transposit IDE

--- src/webhook.js
+++ src/webhook.js
@@ -9,7 +9,8 @@
       };
 	}
 	else {
-      api.run('this.helpdesk', {trigger_id: body.trigger_id});
+      return api.run("slack_bot.open_dialog", { $body: { trigger_id, dialog: body.trigger_id }});
+      //api.run('this.helpdesk', {trigger_id: body.trigger_id});
 	}
   return { status_code: 200 };
 }
