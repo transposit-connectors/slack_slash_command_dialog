dabacbcf881db76d28ba385453758ca128e10c68
Griffin Solot-Kehl <griffin@transposit.com>
2019-07-08 18:13:56 +0000

    Commit from Transposit IDE

--- src/webhook.js
+++ src/webhook.js
@@ -1,16 +1,15 @@
 ({ http_event }) => {
-	 let body = http_event.parsed_body;
+	let body = http_event.parsed_body;
 	if (body.challenge) {
-      // https://api.slack.com/events/url_verification
-      return {
-      status_code: 200,
-      headers: { "Content-Type": "text/plain" },
-      body: body.challenge
-      };
+	// https://api.slack.com/events/url_verification
+	return {
+	status_code: 200,
+	headers: { "Content-Type": "text/plain" },
+	body: body.challenge
+	};
 	}
 	else {
-      return api.run("slack_bot.open_dialog", { $body: { trigger_id, dialog: body.trigger_id }});
-      //api.run('this.helpdesk', {trigger_id: body.trigger_id});
+		api.run('this.helpdesk', {trigger_id: body.trigger_id});
 	}
-  return { status_code: 200 };
+	return { status_code: 200 };
 }
