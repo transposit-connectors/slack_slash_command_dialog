57e2947bb9f98b639aa62b9a87131d051d139d6c
Griffin Solot-Kehl <griffin@transposit.com>
2019-07-09 00:15:45 +0000

    Commit from Transposit IDE

--- src/webhook.js
+++ src/webhook.js
@@ -1,5 +1,5 @@
 ({ http_event }) => {
   let body = http_event.parsed_body;
-  api.run('this.open_dialog', {trigger_id: body.trigger_id});
+  api.run('this.open_dialog', {trigger_id: body.trigger_id, text: body.text});
   return { status_code: 200 };
 }
