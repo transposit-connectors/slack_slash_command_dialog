4005da4efdae3df5a3ea0623a5cbdd6d5dc337fd
Griffin Solot-Kehl <griffin@transposit.com>
2019-07-10 19:38:49 +0000

    Commit from Transposit IDE

--- src/ticket_recieved.js
+++ src/ticket_recieved.js
@@ -1,17 +1,18 @@
 ({ http_event }) => {
   let body = JSON.parse(http_event.parsed_body.payload);
-  setImmediate(() => {
-    api.run('this.create_record', {id: body.user.id, email: api.run('this.get_email', {user: body.user.id})[0].email, title: body.submission.title, description: body.submission.description, urgency: body.submission.urgency});
-
-    let entry = api.run('this.get_records', {id: body.user.id}).slice(-1)[0].id;
-    let url = `https://airtable.com/tblk7ryCHBa1uJVy0/viwoi2AplBGupO48j/${entry}?blocks=hide`;
-
-    if (body.submission.description){
-      api.run('this.confirm_ticket', {channel: body.user.id, email: `Ticket created for ${body.user.name}`, title: body.submission.title, description: body.submission.description, urgency: body.submission.urgency, url: url});
-    }
-    else {
-      api.run('this.confirm_ticket', {channel: body.user.id, email: `Ticket created for ${body.user.name}`, title: body.submission.title, description: 'None provided', urgency: body.submission.urgency, url: url});
-    }
-  };
+  
+  //Create the ticket entry in our databse
+  api.run('this.create_record', {id: body.user.id, email: api.run('this.get_email', {user: body.user.id})[0].email, title: body.submission.title, description: body.submission.description, urgency: body.submission.urgency});
+  //Find the entry to set the url
+  let entry = api.run('this.get_records', {id: body.user.id})[0].id;
+  let url = `https://airtable.com/tblk7ryCHBa1uJVy0/viwoi2AplBGupO48j/${entry}?blocks=hide`;
+  //Choose the description and return to user
+  if (body.submission.description) {
+    api.run('this.confirm_ticket', {channel: body.user.id, email: `Ticket created for ${body.user.name}`, title: body.submission.title, description: body.submission.description, urgency: body.submission.urgency, url: url});
+  }
+  else {
+    api.run('this.confirm_ticket', {channel: body.user.id, email: `Ticket created for ${body.user.name}`, title: body.submission.title, description: 'None provided', urgency: body.submission.urgency, url: url});
+  }
+    
   return { status_code: 200 };
 }
