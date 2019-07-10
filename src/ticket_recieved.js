4009ebe76098a87d59e0348698dffe51e0bb9a74
Griffin Solot-Kehl <griffin@transposit.com>
2019-07-10 18:25:23 +0000

    Commit from Transposit IDE

--- src/ticket_recieved.js
+++ src/ticket_recieved.js
@@ -1,13 +1,14 @@
 ({ http_event }) => {
   let body = JSON.parse(http_event.parsed_body.payload);
-  let entry = api.run
+  let entry = api.run('this.get_records', {id: body.user.id})[-1];
   let url = `https://airtable.com/tblk7ryCHBa1uJVy0/viwoi2AplBGupO48j/${entry}?blocks=hide`;
   api.run('this.create_record', {id: body.user.id, email: api.run('this.get_email', {user: body.user.id})[0].email, title: body.submission.title, description: body.submission.description, urgency: body.submission.urgency});
   if (body.submission.description){
-    api.run('this.confirm_ticket', {channel: body.user.id, email: `Ticket created for ${body.user.name}`, title: body.submission.title, description: body.submission.description, urgency: body.submission.urgency});
+    let description = body.submission.description;
   }
   else {
-    api.run('this.confirm_ticket', {channel: body.user.id, email: `Ticket created for ${body.user.name}`, title: body.submission.title, description: 'None provided', urgency: body.submission.urgency});
+    let description = 'None provided';
   }
+  api.run('this.confirm_ticket', {channel: body.user.id, email: `Ticket created for ${body.user.name}`, title: body.submission.title, description: description, urgency: body.submission.urgency, url: url});
   return { status_code: 200 };
 }
