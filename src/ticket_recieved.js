2a02820193483dff3e346bdf05243d2d04687fc5
Griffin Solot-Kehl <griffin@transposit.com>
2019-07-10 18:26:37 +0000

    Commit from Transposit IDE

--- src/ticket_recieved.js
+++ src/ticket_recieved.js
@@ -1,6 +1,6 @@
 ({ http_event }) => {
   let body = JSON.parse(http_event.parsed_body.payload);
-  let entry = api.run('this.get_records', {id: body.user.id})[-1];
+  let entry = (api.run('this.get_records', {id: body.user.id})[-1]).id;
   let description = 'None provided';
   let url = `https://airtable.com/tblk7ryCHBa1uJVy0/viwoi2AplBGupO48j/${entry}?blocks=hide`;
   api.run('this.create_record', {id: body.user.id, email: api.run('this.get_email', {user: body.user.id})[0].email, title: body.submission.title, description: body.submission.description, urgency: body.submission.urgency});
