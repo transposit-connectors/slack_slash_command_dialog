337bd7712ecaa15ab358327eb1576b2bb0c26ebc
Griffin Solot-Kehl <griffin@transposit.com>
2019-07-10 18:03:48 +0000

    Commit from Transposit IDE

--- src/ticket_recieved.js
+++ src/ticket_recieved.js
@@ -1,10 +1,11 @@
 ({ http_event }) => {
   let body = JSON.parse(http_event.parsed_body.payload);
+  api.run('this.create_record', {id: body.user.id, email: api.run('this.get_email', {user: body.user.id})[0].email, title: body.submission.title, description: body.submission.description, urgency: body.submission.urgency});
   if (body.submission.description){
     api.run('this.confirm_ticket', {channel: body.user.id, email: `Ticket created for ${body.user.name}`, title: body.submission.title, description: body.submission.description, urgency: body.submission.urgency});
   }
   else {
     api.run('this.confirm_ticket', {channel: body.user.id, email: `Ticket created for ${body.user.name}`, title: body.submission.title, description: 'None provided', urgency: body.submission.urgency});
   }
-  return { status_code: 200 };
+  return body;{ status_code: 200 };
 }
