f06b3777fe0fc241243cb727c2c05f0b0e8f8327
Griffin Solot-Kehl <griffin@transposit.com>
2019-07-12 20:31:15 +0000

    Commit from Transposit IDE

--- src/ticket_recieved.js
+++ src/ticket_recieved.js
@@ -5,6 +5,7 @@
   api.run('this.create_record', {id: body.user.id, email: api.run('this.get_email', {user: body.user.id})[0].email, title: body.submission.title, description: body.submission.description, urgency: body.submission.urgency});
   //Find the entry to set the url
   let entry = api.run('this.get_records', {id: body.user.id}).pop().id;
+  //Change this url to your Airtable location
   let url = `https://airtable.com/tblk7ryCHBa1uJVy0/viwoi2AplBGupO48j/${entry}?blocks=hide`;
   //Choose the description and return to user
   if (body.submission.description) {
