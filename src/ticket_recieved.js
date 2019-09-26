1ac73882668239cc186675acb94325962739ec16
Griffin Solot-Kehl <griffin@transposit.com>
2019-09-26 17:44:04 +0000

    Commit from Transposit IDE

--- src/ticket_recieved.js
+++ src/ticket_recieved.js
@@ -1,36 +1,37 @@
 ({ http_event }) => {
-  let body = JSON.parse(http_event.parsed_body.payload);
-  
-  // Create the ticket entry in our databse
-  api.run('this.create_record', {
-    							  baseId: env.get("baseId"),
-                                  id: body.user.id,
-                                  email: api.query('SELECT user.profile.email FROM slack.get_users_info WHERE user=@user', 
-                          			{user: body.user.id})[0].email,
-                                  title: body.submission.title,
-                                  description: body.submission.description,
-                                  urgency: body.submission.urgency
-                                });
-  // Choose the description and return to user
-  if (body.submission.description) {
-    api.run('this.confirm_ticket', {
-      								channel: body.user.id,
-                                    email: `Ticket created for ${body.user.name}`,
+  setImmediate(() => {
+    let body = JSON.parse(http_event.parsed_body.payload);
+
+    // Create the ticket entry in our databse
+    api.run('this.create_record', {
+                                    baseId: env.get("baseId"),
+                                    id: body.user.id,
+                                    email: api.query('SELECT user.profile.email FROM slack.get_users_info WHERE user=@user', 
+                                      {user: body.user.id})[0].email,
                                     title: body.submission.title,
                                     description: body.submission.description,
-                                    urgency: body.submission.urgency,
-                                    url: env.get("url")
-                                   });
-  } else {
-    api.run('this.confirm_ticket', {
-      								channel: body.user.id,
-                                    email: `Ticket created for ${body.user.name}`,
-                                    title: body.submission.title,
-                                    description: 'None provided',
-                                    urgency: body.submission.urgency,
-                                    url: env.get("url")
-                                   });
-  }
-    
+                                    urgency: body.submission.urgency
+                                  });
+    // Choose the description and return to user
+    if (body.submission.description) {
+      api.run('this.confirm_ticket', {
+                                      channel: body.user.id,
+                                      email: `Ticket created for ${body.user.name}`,
+                                      title: body.submission.title,
+                                      description: body.submission.description,
+                                      urgency: body.submission.urgency,
+                                      url: env.get("url")
+                                     });
+    } else {
+      api.run('this.confirm_ticket', {
+                                      channel: body.user.id,
+                                      email: `Ticket created for ${body.user.name}`,
+                                      title: body.submission.title,
+                                      description: 'None provided',
+                                      urgency: body.submission.urgency,
+                                      url: env.get("url")
+                                     });
+    }
+  });
   return { status_code: 200 };
 }
